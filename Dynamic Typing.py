367d487814a8d27edc4bac1171777f4a61eecb27
JHMamun <gregorian.zahid@gmail.com>
2021-06-02 01:21:29 +0600

    strings

--- Dynamic Typing.py
+++ Dynamic Typing.py
@@ -1,3 +1,22 @@
 students_count = 1000
 students_count: int
 print(type(students_count))
+
+age: int = 20
+age = "Python"
+print(age)
+# mutable types
+x = 1
+print(id(x))
+
+x = x + 1
+print(id(x))
+
+# immutable types
+x = [1, 2, 3]
+print(id(x))
+x.append(4)
+print(id(x))
+
+course = "Python Programming"
+print(len(course))
