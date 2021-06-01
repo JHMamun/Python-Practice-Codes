f80280cdae62586d110905efcf7f6f7219703140
JHMamun <gregorian.zahid@gmail.com>
2021-06-02 01:49:37 +0600

    escape characters

--- Dynamic Typing.py
+++ Dynamic Typing.py
@@ -18,5 +18,33 @@ print(id(x))
 x.append(4)
 print(id(x))
 
+# strings are immutable
 course = "Python Programming"
 print(len(course))
+print(course[0])
+print(course[-1])
+print(course[-2])
+
+print(course[0:3])  # slicing Pyt from Python Programming
+print(course[:3])
+print(course[0:])  # returns entire string
+print(course[:])  # returns entire string
+
+print(id(course))
+print(id(course[0]))
+
+
+# escape character
+message = 'Python "Programming'
+print(message)
+message = "Python \"Programming"
+print(message)
+message = "Python \nProgramming"
+print(message)
+
+# use of triple quote
+message = """
+Python
+Programming
+"""
+print(message)
