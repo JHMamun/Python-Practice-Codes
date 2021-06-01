98573c25bff6a2b8bfc74ace035564916725261e
JHMamun <gregorian.zahid@gmail.com>
2021-06-02 02:25:27 +0600

    Update logicaloperator.py

---
+++ logicaloperator.py
@@ -0,0 +1,12 @@
+name = "Mosh"
+
+if not name:
+    print("name is empty")
+elif name:
+    print("name is not empty")
+
+name = ""
+if not name:
+    print("name is empty")
+elif name:
+    print("name is not empty")
