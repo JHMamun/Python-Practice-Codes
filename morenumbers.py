eba9cc0b21bb93ce59d5f4427c2a15963a014de6
JHMamun <gregorian.zahid@gmail.com>
2021-06-02 02:14:36 +0600

    boolandype

---
+++ morenumbers.py
@@ -0,0 +1,8 @@
+import math
+
+PI = 3.14
+print(round(PI))
+PI = -3.14
+print(abs(PI))
+
+print(math.floor(PI))
---
+++ typeconversion.py
@@ -0,0 +1,13 @@
+x = input("x: ")
+#y = x + 1
+
+print(int(x))
+print(float(x))
+print(bool(x))
+
+# Falsy
+# ""
+# 0
+# []
+# None(null)
+# Only these values will give Flase bool value
